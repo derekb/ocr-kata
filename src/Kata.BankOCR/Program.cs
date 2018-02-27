06ae2c59e93c995fc79f7f9987a44a1ac199b62c
Derek <derek.burkgren@gmail.com>
2018-02-27 09:57:10 -0800

    Extract each entry into separate grouping

--- src/Kata.BankOCR/Program.cs
+++ src/Kata.BankOCR/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Kata.BankOCR
 {
@@ -8,6 +11,15 @@ namespace Kata.BankOCR
       {
          if (args.Length != 1)
             Console.WriteLine("Usage: ocr-kata </path/to/file>");
+
+         var filePath = args.Single();
+
+         var entries =
+            File
+               .ReadAllLines(filePath)
+               .Select((line, idx) => new {Line = line, Idx = idx})
+               .GroupBy(item => item.Idx / 4)
+               .Select(x => x.Select(y => y.Line).ToArray());
       }
    }
 }
