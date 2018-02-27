6933a546e8be2212dbbaecc002d04ceb167e5491
Derek <derek.burkgren@gmail.com>
2018-02-27 10:50:11 -0800

    Add class to detect valid characters

---
+++ src/Kata.BankOCR.Test/Representations/ValidCharactersTest.cs
@@ -0,0 +1,31 @@
+﻿using Kata.BankOCR.Representations;
+using NUnit.Framework;
+
+namespace Kata.BankOCR.Test.Representations
+{
+   [TestFixture]
+   public class ValidCharactersTest
+   {
+      [Test]
+      public void ZeroIsAValidCharacter()
+      {
+         var rawEntry =
+            " _ " +
+            "| |" +
+            "|_|";
+
+         Assert.That(ValidCharacters.IsValidCharacter(rawEntry), Is.True);
+      }
+
+      [Test]
+      public void ZeroAsRawInputYieldsZero()
+      {
+         var rawEntry =
+            " _ " +
+            "| |" +
+            "|_|";
+
+         Assert.That(ValidCharacters.ToCharacter(rawEntry), Is.EqualTo('0'));
+      }
+   }
+}
---
+++ src/Kata.BankOCR/Representations/ValidCharacters.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.BankOCR.Representations
+{
+   public class ValidCharacters
+   {
+      private static readonly Dictionary<string, char> validCharacters = 
+         new Dictionary<string, char>
+         {
+            { "   " +
+              "  |" +
+              "  |", '1'},
+            { " _ " +
+              " _|" +
+              "|_ ", '2'},
+            { " _ " +
+              " _|" +
+              " _|", '3'},
+            { "   " +
+              "|_|" +
+              "  |", '4'},
+            { " _ " +
+              "|_ " +
+              " _|", '5'},
+            { " _ " +
+              "|_ " +
+              "|_|", '6'},
+            { " _ " +
+              "  |" +
+              "  |", '7'},
+            { " _ " +
+              "|_|" +
+              "|_|", '8'},
+            { " _ " +
+              "|_|" +
+              " _|", '9'},
+            { " _ " +
+              "| |" +
+              "|_|", '0'},
+
+         };
+      public static bool IsValidCharacter(string input)
+      {
+         return validCharacters.ContainsKey(input);
+      }
+      public static char ToCharacter(string input)
+      {
+         return validCharacters[input];
+      }
+   }
+}
