36a5004b6b5eb8feb144ec3694c1c273495aa5a6
Derek <derek.burkgren@gmail.com>
2018-02-28 08:08:16 -0800

    Fill Entry with lines and expose them as text

---
+++ src/Kata.BankOCR.Test/Representations/EntryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Kata.BankOCR.Parsers;
+using Kata.BankOCR.Representations;
+using NUnit.Framework;
+
+namespace Kata.BankOCR.Test.Representations
+{
+   [TestFixture]
+   public class EntryTest
+   {
+      [Test]
+      public void EntryShouldParseToCharacters()
+      {
+         var rawEntry =
+            new[]
+            {
+               " _  _  _  _  _  _  _  _  _ ",
+               "| || || || || || || || || |",
+               "|_||_||_||_||_||_||_||_||_|",
+               "\r\n"
+            };
+
+         var entry = Entry.FromLines(rawEntry, 3);
+
+         Assert.That(entry.AsText(), Is.EqualTo("000000000"));
+      }
+   }
+}
--- src/Kata.BankOCR/Representations/Entry.cs
+++ src/Kata.BankOCR/Representations/Entry.cs
@@ -1,9 +1,39 @@
-﻿namespace Kata.BankOCR.Representations
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.BankOCR.Representations
 {
     public class Entry
     {
-       private Entry () { }
+       private string[] lines;
+       private readonly int _charWidth;
 
-       public static Entry FromLines(string[] lines) => new Entry();
+       private Entry(string[] lines, int charWidth)
+       {
+          this.lines = lines;
+          _charWidth = charWidth;
+       }
+
+       public static Entry FromLines(string[] lines, int charWidth) => new Entry(lines, charWidth);
+
+       public string AsText()
+       {
+          return new string(GetCharacters().ToArray());
+       }
+
+       private IEnumerable<char> GetCharacters()
+       {
+          for (int i = 0; i < lines[0].Length; i += _charWidth)
+          {
+             yield return
+                ValidCharacters
+                   .ToCharacter(
+                      lines[0].Substring(i, _charWidth) +
+                      lines[1].Substring(i, _charWidth) +
+                      lines[2].Substring(i, _charWidth)
+                      );
+          }
+       }
     }
 }
