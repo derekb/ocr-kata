70174de0c22fe630616fa1f5a81a6ae76dca26b2
Derek <derek.burkgren@gmail.com>
2018-02-28 09:13:58 -0800

    Add illegible character support to main program

--- src/Kata.BankOCR/Program.cs
+++ src/Kata.BankOCR/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Kata.BankOCR.Parsers;
+using Kata.BankOCR.Representations;
 
 namespace Kata.BankOCR
 {
@@ -18,7 +19,14 @@ namespace Kata.BankOCR
          var entries = EntryParser.Parse(filePath, 3);
 
          foreach(var entry in entries)
-            Console.WriteLine(entry.AsText());
+            Console.WriteLine(PrintEntry(entry));
+      }
+
+      private static string PrintEntry(Entry entry)
+      {
+         var accountNumber = entry.AsText();
+         var illegible = accountNumber.Contains("?") ? " ILL" : String.Empty;
+         return $"{accountNumber}{illegible}";
       }
    }
 }
